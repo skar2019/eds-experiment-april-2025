9a0048ed7e279a153122aa627aac1d333ca8de85
Suman Kar <skar@Sumans-MacBook-Pro.local>
2025-04-12 10:33:07 +0530

    Sitekit modified

--- scripts/configs.js
+++ scripts/configs.js
@@ -1,8 +1,7 @@
-import { getMetadata } from './aem.js';
-
-/* eslint-disable import/no-cycle */
+import { getHref, getOrigin } from './scripts.js';
+ 
 const ALLOWED_CONFIGS = ['prod', 'stage', 'dev'];
-
+ 
 /**
  * This function calculates the environment in which the site is running based on the URL.
  * It defaults to 'prod'. In non 'prod' environments, the value can be overwritten using
@@ -11,7 +10,8 @@ const ALLOWED_CONFIGS = ['prod', 'stage', 'dev'];
  * @returns {string} - environment identifier (dev, stage or prod'.
  */
 export const calcEnvironment = () => {
-  const { host, href } = window.location;
+  const href = getHref();
+  const host = getOrigin();
   let environment = 'prod';
   if (href.includes('.aem.page') || host.includes('staging')) {
     environment = 'stage';
@@ -19,7 +19,7 @@ export const calcEnvironment = () => {
   if (href.includes('localhost')) {
     environment = 'dev';
   }
-
+ 
   const environmentFromConfig = window.sessionStorage.getItem('environment');
   if (
     environmentFromConfig
@@ -28,68 +28,35 @@ export const calcEnvironment = () => {
   ) {
     return environmentFromConfig;
   }
-
+ 
   return environment;
 };
-
+ 
 function buildConfigURL(environment) {
   const env = environment || calcEnvironment();
   let fileName = 'configs.json';
   if (env !== 'prod') {
     fileName = `configs-${env}.json`;
   }
-  const configURL = new URL(`${window.location.origin}/${fileName}`);
+  const configURL = new URL(`${getOrigin()}/${fileName}`);
   return configURL;
 }
-
-function applyConfigOverrides(config) {
-  // get overrides
-  const website = getMetadata('commerce-website');
-  const store = getMetadata('commerce-store');
-  const storeview = getMetadata('commerce-storeview');
-
-  // add overrides
-  const updates = new Map([
-    ['commerce.headers.cs.Magento-Website-Code', website],
-    ['commerce.headers.cs.Magento-Store-Code', store],
-    ['commerce.headers.cs.Magento-Store-View-Code', storeview],
-    ['commerce.headers.all.Store', storeview],
-  ]);
-
-  // apply updates
-  config.data.forEach((item) => {
-    const next = updates.get(item.key);
-    if (next) {
-      item.value = next;
-      updates.delete(item.key);
-    }
-  });
-
-  // add any updates that weren't applied
-  updates.forEach((value, key) => {
-    if (value) {
-      config.data.push({ key, value });
-    }
-  });
-
-  return config;
-}
-
+ 
 const getConfigForEnvironment = async (environment) => {
   const env = environment || calcEnvironment();
-
+ 
   try {
     const configJSON = window.sessionStorage.getItem(`config:${env}`);
     if (!configJSON) {
       throw new Error('No config in session storage');
     }
-
+ 
     const parsedConfig = JSON.parse(configJSON);
     if (!parsedConfig[':expiry'] || parsedConfig[':expiry'] < Math.round(Date.now() / 1000)) {
       throw new Error('Config expired');
     }
-
-    return applyConfigOverrides(parsedConfig);
+ 
+    return parsedConfig;
   } catch (e) {
     let configJSON = await fetch(buildConfigURL(env));
     if (!configJSON.ok) {
@@ -98,10 +65,10 @@ const getConfigForEnvironment = async (environment) => {
     configJSON = await configJSON.json();
     configJSON[':expiry'] = Math.round(Date.now() / 1000) + 7200;
     window.sessionStorage.setItem(`config:${env}`, JSON.stringify(configJSON));
-    return applyConfigOverrides(configJSON);
+    return configJSON;
   }
 };
-
+ 
 /**
  * This function retrieves a configuration value for a given environment.
  *
@@ -115,45 +82,37 @@ export const getConfigValue = async (configParam, environment) => {
   const configElements = config.data;
   return configElements.find((c) => c.key === configParam)?.value;
 };
-
+ 
 /**
  * Retrieves headers from config entries like commerce.headers.pdp.my-header, etc and
  * returns as object of all headers like { my-header: value, ... }
- */
+*/
 export const getHeaders = async (scope, environment) => {
   const env = environment || calcEnvironment();
   const config = await getConfigForEnvironment(env);
-  const configElements = config.data.filter((el) => el?.key.includes('headers.all') || el?.key.includes(`headers.${scope}`));
-
+  const configElements = config.data.filter((el) => el?.key.includes(`headers.${scope}`));
+ 
   return configElements.reduce((obj, item) => {
     let { key } = item;
-
-    // global values
-    if (key.includes('commerce.headers.all.')) {
-      key = key.replace('commerce.headers.all.', '');
-    }
-
-    // scoped values
     if (key.includes(`commerce.headers.${scope}.`)) {
       key = key.replace(`commerce.headers.${scope}.`, '');
     }
-
     return { ...obj, [key]: item.value };
   }, {});
 };
-
+ 
 export const getCookie = (cookieName) => {
   const cookies = document.cookie.split(';');
   let foundValue;
-
+ 
   cookies.forEach((cookie) => {
     const [name, value] = cookie.trim().split('=');
     if (name === cookieName) {
       foundValue = decodeURIComponent(value);
     }
   });
-
+ 
   return foundValue;
 };
-
+ 
 export const checkIsAuthenticated = () => !!getCookie('auth_dropin_user_token') ?? false;
